456b90678b33146ea308eecc85ef7b1372b8e72c
Iris Hu <ihu009@gmail.com>
2019-07-16 16:38:36 -0700

    started form django stuff ugh

--- TweetValidator/settings.py
+++ TweetValidator/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Uploaded meda
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- TweetValidator/views.py
+++ TweetValidator/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
