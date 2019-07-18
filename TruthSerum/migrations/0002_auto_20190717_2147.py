8ba109f8f43e0bd418173d52ecce1b7e97f9f0f0
Uzoma Nwakuche <unwakuche@dropbox.com>
2019-07-17 17:46:39 -0700

    Rebranding

--- TruthSerum/migrations/0002_auto_20190717_2147.py
+++ TruthSerum/migrations/0002_auto_20190717_2147.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('TweetValidator', '0001_initial'),
+        ('TruthSerum', '0001_initial'),
     ]
 
     operations = [
--- TruthSerum/wsgi.py
+++ TruthSerum/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TweetValidator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TruthSerum.settings')
 
 application = get_wsgi_application()
